8bc8707a959eff76613b6102b6975f839edfdb43
Zer0-M <msj2340@gmail.com>
2018-10-03 22:14:09 -0400

    the string is changed by changing individual characters

--- construction.c
+++ construction.c
@@ -1,56 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "construction.h"
 
 struct phone {int price; char *name;};
 
 struct phone newphone(int i) {
-	struct phone output;
-	if (i == 0) {
-		output.price = 50;
-		output.name = "flipphone";
-	}
-	else if (i == 1) {
-		output.price = 1000;
-		output.name = "iphone";
-	}
-	else {
-		output.price = 700;
-		output.name = "android";
-	}
-	return output;
+  struct phone output;
+  if (i == 0) {
+    output.price = 50;
+    output.name = "flipphone";
+  }
+  else if (i == 1) {
+    output.price = 1000;
+    output.name = "iphone";
+  }
+  else {
+    output.price = 700;
+    output.name = "android";
+  }
+  return output;
 }
 
 void printphone(struct phone phonevar) {
-	printf("Price of %s: %d\n", phonevar.name, phonevar.price);
+  printf("Price of %s: %d\n", phonevar.name, phonevar.price);
 
 }
 
 void changeprice(struct phone *phonevar, int change) {
-	phonevar->price += change;
+  phonevar->price += change;
 }
 
 void renamephone(struct phone *phonevar, char *newname) {
-	phonevar->name = newname;
+  //phonevar->name = newname;
+  char temp[100];
+  for(int i=0;i<strlen(phonevar->name);i++){
+    temp[i]=phonevar->name[i];
+  }
+  for(int i=0;i<strlen(temp);i++){
+    temp[i]=newname[i];
+  }
+  phonevar->name=temp;
 }
 
 int main() {
-	struct phone phonevar;
-	printf("Testing creating phones and changing price.\n");
-	for (int i = 0; i< 3; i++) {
-		phonevar = newphone(i);
-		printf("Before:\n");
-		printphone(phonevar);
-		changeprice(&phonevar, 10 *i + 10);
-		printf("After:\n");
-		printphone(phonevar);
-	}
-	printf("\nTesting changing name:\n");
-	printf("Before:\n");
-	printphone(phonevar);
-	renamephone(&phonevar, "samsung");
-	printf("After:\n");
-	printphone(phonevar);
+  struct phone phonevar;
+  printf("Testing creating phones and changing price.\n");
+  for (int i = 0; i< 3; i++) {
+    phonevar = newphone(i);
+    printf("Before:\n");
+    printphone(phonevar);
+    changeprice(&phonevar, 10 *i + 10);
+    printf("After:\n");
+    printphone(phonevar);
+  }
+  printf("\nTesting changing name:\n");
+  printf("Before:\n");
+  printphone(phonevar);
+  renamephone(&phonevar, "samsung");
+  printf("After:\n");
+  printphone(phonevar);
 
 }
 
